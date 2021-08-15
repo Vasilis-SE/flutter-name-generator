eeaa58178414b209cce6f02d5f9708f406d37d74
Vasilis CodeX <vasilis.tris@gmail.com>
2021-08-15 12:15:23 +0300

    secure storage service script

--- flutter_name_generator/lib/widgets/randomWords.dart
+++ flutter_name_generator/lib/widgets/randomWords.dart
@@ -1,5 +1,8 @@
+// External 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
+// Internal
 import 'navDrawer.dart';
 
 class RandomWords extends StatefulWidget {
@@ -14,16 +17,17 @@ class _RandomWordsState extends State<RandomWords> {
 
   Widget _buildSuggestions() {
     return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
 
-          final index = i ~/ 2;
-          if (index >= _suggestions.length)
-            _suggestions.addAll(generateWordPairs().take(10));
+        final index = i ~/ 2;
+        if (index >= _suggestions.length)
+          _suggestions.addAll(generateWordPairs().take(10));
 
-          return _buildRow(_suggestions[index]);
-        });
+        return _buildRow(_suggestions[index]);
+      }
+    );
   }
 
   Widget _buildRow(WordPair randoName) {
@@ -99,7 +103,7 @@ class _RandomWordsState extends State<RandomWords> {
         // actions: [IconButton(onPressed: _pushSaved, icon: Icon(Icons.list))],
       ),
       drawer: NavDrawer(),
-      body: _buildSuggestions(),
+      body: _buildSuggestions()
     );
   }
 }
